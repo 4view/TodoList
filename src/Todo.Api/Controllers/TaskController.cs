561ee79a41bf27240f76f254c48ae82da2d84538
Кочергин А.А. <aa.kochergin@corp.tensor.ru>
2025-02-28 14:01:15 +0500

    feat: Add challenge controllers

--- src/Todo.Api/Controllers/TaskController.cs
+++ src/Todo.Api/Controllers/TaskController.cs
@@ -1,8 +1,61 @@
+using System.Net;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Todo.Core.Models;
+using Todo.Data;
 
 namespace Todo.Api.Controllers;
 
 public class TaskController : Controller
 {
+    private ApplicationDBContext _db;
+    public TaskController(ApplicationDBContext db)
+    {
+        _db = db;
+    }
+
+    [HttpGet]
+    public IActionResult GetAllChallenges()
+    {
+        var listChallenges = ChallengesList();
+
+        return Json(listChallenges);
+    }
+
+    [HttpPost]
+    public IActionResult AddChallenge(Challenge challenge)
+    {
+        if (ModelState.IsValid)
+        {
+            _db.Set<Challenge>().Add(challenge);
+            _db.SaveChanges();
+        }
+
+        var listChallenges = ChallengesList();
+
+        return Json(listChallenges);
+    }
+
+    [HttpDelete]
+    public IActionResult DeleteChallenge(Guid id)
+    {
+        var selectedChallenge = _db.Set<Challenge>().Find(id);
+
+        if (selectedChallenge == null)
+            return NotFound();
+
+        _db.Set<Challenge>().Remove(selectedChallenge);
+        _db.SaveChanges();
+
+        var listChallenges = ChallengesList();
+
+        return Json(listChallenges); 
+    }
     
+    private List<Challenge> ChallengesList()
+    {
+        var listData = _db.Set<Challenge>().ToList();
+
+        return listData;
+    }
 }
