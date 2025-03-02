31c5ab70c5f1c99db8e6d4caf98e9dbe1adc93a9
4view <akochergin.2002@gmail.com>
2025-03-02 16:15:41 +0500

    feat: Add connection string to DB

--- src/Todo.Api/Program.cs
+++ src/Todo.Api/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Todo.Data;
+
 var builder = WebApplication.CreateBuilder();
+
+builder.Services.AddDbContext<ApplicationDBContext>(
+    o => o.UseNpgsql(builder.Configuration.GetConnectionString("TodoListDB"))
+);
+
 var app = builder.Build();
 
 app.Run();
