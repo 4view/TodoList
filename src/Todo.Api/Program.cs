0f27cb05e047bfe5abfae905aebe0ddf328a939f
4view <akochergin.2002@gmail.com>
2025-03-06 22:49:00 +0500

    feat: Add routing

--- src/Todo.Api/Program.cs
+++ src/Todo.Api/Program.cs
@@ -3,10 +3,16 @@ using Todo.Data;
 
 var builder = WebApplication.CreateBuilder();
 
+builder.Services.AddControllers();
+
 builder.Services.AddDbContext<ApplicationDBContext>(
     o => o.UseNpgsql(builder.Configuration.GetConnectionString("TodoListDB"))
 );
 
 var app = builder.Build();
+
+app.UseRouting();
+
+app.MapControllers();
 
 app.Run();
