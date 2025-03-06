e4c95ada0dba730c3bb6902d7cd1a69403790c25
4view <akochergin.2002@gmail.com>
2025-03-06 22:46:13 +0500

    feat: Change properties configure and add new migration

--- src/Todo.Data/Configurations/ChallengeConfiguration.cs
+++ src/Todo.Data/Configurations/ChallengeConfiguration.cs
@@ -10,8 +10,8 @@ public class ChallengeConfiguration : IEntityTypeConfiguration<ChallengeEntity>
     public void Configure(EntityTypeBuilder<ChallengeEntity> builder)
     {
         builder.HasKey(b => b.Id);
-        builder.Property(b => b.Comment);
-        builder.Property(b => b.CreationDate);
-        builder.Property(b => b.isComplete);
+        builder.Property(b => b.Comment).HasMaxLength(500);
+        builder.Property(b => b.CreationDate).IsRequired();
+        builder.Property(b => b.isComplete).HasDefaultValue(false).IsRequired();
     }
 }
---
+++ src/Todo.Data/Migrations/20250306172048_Update.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Todo.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "isComplete",
+                table: "ChallengeEntity",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                table: "ChallengeEntity",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "isComplete",
+                table: "ChallengeEntity",
+                type: "boolean",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldDefaultValue: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                table: "ChallengeEntity",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+        }
+    }
+}
